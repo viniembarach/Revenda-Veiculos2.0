7c595eab40bb0b801ac9be1768e3e04835d1f1cc
Marcos Vinicius Bedin Embarach <viniembarach12@gmail.com>
2023-10-26 11:42:09 -0300

    Ajustes na criação das tabelas

--- sistemaRevendaVeiculos/src/main/resources/db/migration/V1__create_table_pessoa.sql
+++ sistemaRevendaVeiculos/src/main/resources/db/migration/V1__create_table_pessoa.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `pessoa` (
  `id`        bigint                 NOT NULL PRIMARY KEY AUTO_INCREMENT,
- `cpfoucnpj` varchar(20)            NOT NULL,
+ `cpfoucnpj` bigint                 NOT NULL,
  `nome`      varchar(100)           NOT NULL,
- `telefone`  varchar(20)            NOT NULL,
+ `telefone`  bigint                 NOT NULL,
  `cidade`    varchar(100)           NOT NULL,
  `endereco`  varchar(255)           NOT NULL,
  `tipo` enum('CLIENTE', 'VENDEDOR') DEFAULT NULL
